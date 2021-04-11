d2123f5155ca8953f34170665050a4ee2a0fce58
Leandro Freitas <freitass@gmail.com>
2021-04-10 22:12:44 -0400

    Fix configuration issues with Xcode

--- test-suite/rpath_hack.py
+++ test-suite/rpath_hack.py
@@ -6,7 +6,7 @@ pattern = "${CMAKE_CURRENT_BINARY_DIR}/PyCFFIlib_cffi*.so"
 for so in glob.glob(pattern):
     print("rpath hack %s" % (so))
     rpath_hack = subprocess.run(
-        ["${CMAKE_INSTALL_NAME_TOOL}", "-change", "@rpath/$<TARGET_FILE_NAME:mylib>", "$<TARGET_FILE:mylib>", so])
+        ["${CMAKE_INSTALL_NAME_TOOL}", "-change", "@rpath/$<TARGET_FILE_NAME:mylib>", "${CMAKE_CURRENT_BINARY_DIR}/$<TARGET_FILE_NAME:mylib>", so])
     if rpath_hack.returncode != 0:
         sys.exit(rpath_hack.returncode)
 
